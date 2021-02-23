f66e41a1ad1e620ad5ab20204445ebdea3768d43
Saddam Hussain <saddam.hussain@kwanso.com>
2021-02-23 19:08:22 +0500

    Added support for all country states

---
+++ data/index.js
@@ -0,0 +1,13 @@
+const bulkify = require('bulk-require')
+const _ = require('lodash')
+
+module.exports = function () {
+  const totalList = []
+  const fileList = bulkify(__dirname, ['*.json'])
+  _.forEach(fileList, function (file) {
+    // file.ISO[2] = file.ISO.alpha2
+    // file.ISO[3] = file.ISO.alpha3
+    totalList.push(file)
+  })
+  return totalList
+}
