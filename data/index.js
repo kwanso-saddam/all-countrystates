a648c587346d2b140ba18092c0d18abd4c6a6a41
Saddam Hussain <saddam.hussain@kwanso.com>
2021-02-25 19:55:20 +0500

    Added missing states

--- data/index.js
+++ data/index.js
@@ -1,13 +1,13 @@
-const bulkify = require('bulk-require')
-const _ = require('lodash')
+const bulkify = require("bulk-require");
+const _ = require("lodash");
 
 module.exports = function () {
-  const totalList = []
-  const fileList = bulkify(__dirname, ['*.json'])
+  const totalList = [];
+  const fileList = bulkify(__dirname, ["*.json"]);
   _.forEach(fileList, function (file) {
     // file.ISO[2] = file.ISO.alpha2
     // file.ISO[3] = file.ISO.alpha3
-    totalList.push(file)
-  })
-  return totalList
-}
+    totalList.push(file);
+  });
+  return totalList;
+};
